1cba088c4495b26e0aa6e1755f1648c7a0f3532f
Michael Orwat <morwat9@hotmail.com>
2018-03-31 05:34:30 -0700

    Created a home page. Added some modules, took out some of the hackers stuff.

--- sabio.node.starter/content/src/App.js
+++ sabio.node.starter/content/src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './containers/homepage'
+import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Route, Switch, Link } from 'react-router-dom'
 
-import {BrowserRouter, Route} from 'react-router-dom'
-import Hackers from './containers/Hackers'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <BrowserRouter>
-          <Route path="/" component={Hackers}/>
-        </BrowserRouter>
+
+        <Navbar id="navbar" collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/'>Chime In</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+
+              <LinkContainer to='/example1'>
+                <NavItem>
+                  Public Quizzes
+                  </NavItem>
+              </LinkContainer>
+
+              <LinkContainer to='/example2'>
+                <NavItem>
+                  Private Quizzes
+                </NavItem>
+              </LinkContainer>
+
+              <LinkContainer to='/example3'>
+                <NavItem>
+                  Another Thing?
+                </NavItem>
+              </LinkContainer>
+
+            </Nav>
+            <Nav pullRight>
+              <NavDropdown eventKey={3} title="USERNAME HERE" id="basic-nav-dropdown">
+                <MenuItem>My Private Quizzes</MenuItem>
+                <MenuItem>My Public Quizzes</MenuItem>
+                <MenuItem divider />
+                <MenuItem>Logout</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Switch>
+          <Route path='/' component={Homepage}></Route>
+        </Switch>
       </div>
     );
   }
