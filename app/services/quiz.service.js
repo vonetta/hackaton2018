09e00c5cebcf767a35bde75dda041d09a7196420
Alex <37161837+AlexVanilla@users.noreply.github.com>
2018-04-01 04:56:38 -0700

    almost finish quizForm

--- app/services/quiz.service.js
+++ app/services/quiz.service.js
@@ -3,41 +3,41 @@ const conn = mongodb.connection
 const ObjectId = mongodb.ObjectId
 
 module.exports = {
-  create: _create,
-  read: read
+    create: _create,
+    read: read
 }
 
 function read() {
-  return conn
-    .db()
-    .collection("public-quizzes")
-    .find({})
-    .toArray()
-    .then(quizzes => {
-      for (let i = 0; i < quizzes.length; i++) {
-        let quiz = quizzes[i]
-        quiz._id = quiz._id.toString()
-      }
-      return quizzes
-    })
-    .catch(err => {
-      return Promise.reject(err)
-    })
+    return conn
+        .db()
+        .collection("private-quizzes")
+        .find({})
+        .toArray()
+        .then(quizzes => {
+            for (let i = 0; i < quizzes.length; i++) {
+                let quiz = quizzes[i]
+                quiz._id = quiz._id.toString()
+            }
+            return quizzes
+        })
+        .catch(err => {
+            return Promise.reject(err)
+        })
 }
 
 function _create(payload) {
-  console.log(payload)
-  const doc = {
-    questions: payload.questions,
-    quizName: payload.quizName
-  }
+    console.log(payload)
+    const doc = {
+        questions: payload.questions,
+        quizName: payload.quizName
+    }
 
-  return conn
-    .db()
-    .collection("public-quizzes")
-    .insertOne(doc)
-    .then(result => result.insertedId.toString())
-    .catch(err => {
-      return Promise.reject(err)
-    })
+    return conn
+        .db()
+        .collection("private-quizzes")
+        .insertOne(doc)
+        .then(result => result.insertedId.toString())
+        .catch(err => {
+            return Promise.reject(err)
+        })
 }
