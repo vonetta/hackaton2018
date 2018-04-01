f514a2d2449c9b3763c2f70a9a9bf0e99bed9e3c
Vonetta <vonettastevenson@gmail.com>
2018-03-31 20:09:35 -0700

    get route and post fixes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,21 @@
-const router = require('express').Router()
-const clientRoutes = require('./client.routes')
-const authenticate = require('../filters/authenticate')
+"use strict"
 
-const quizApiPrefix = '/api/quizzes'
-const quizRoutes = require('./quiz.routes')(quizApiPrefix)
+const express = require("express")
 
+const router = require("express").Router()
+const clientRoutes = require("./client.routes")
+const authenticate = require("../filters/authenticate")
 
+const quizApiPrefix = "/api/quizzes"
+const quizRoutes = require("./quiz.routes")(quizApiPrefix)
+
+const path = require("path")
+const contentPath = path.join(__dirname, "../../content")
 
 module.exports = router
 
-
 // check authentication for all requests
+router.use(express.static(contentPath))
 router.use(authenticate)
 
 // API routes (group routing modules here - no empty lines between)
@@ -18,32 +23,29 @@ router.use(quizApiPrefix, quizRoutes)
 // router.use('/api/entities', entitiesRoutes)
 // router.use('/api/examples', examplesRoutes)
 
-
-
 // API error handlers (API routes must be registered before this)
 useAPIErrorHandlers(router)
 
 // register client routes
 router.use(clientRoutes)
 
-
 function useAPIErrorHandlers(router) {
-    // Handle API 404
-    router.use('/api/*', (req, res, next) => {
-        res.sendStatus(404)
-    })
+  // Handle API 404
+  router.use("/api/*", (req, res, next) => {
+    res.sendStatus(404)
+  })
 
-    // Handle API 500
-    router.use((err, req, res, next) => {
-        // If the error object doesn't exists
-        if (!err) {
-            return next()
-        }
+  // Handle API 500
+  router.use((err, req, res, next) => {
+    // If the error object doesn't exists
+    if (!err) {
+      return next()
+    }
 
-        // Log it
-        console.error(err.stack)
+    // Log it
+    console.error(err.stack)
 
-        // Redirect to error page
-        res.sendStatus(500)
-    })
+    // Redirect to error page
+    res.sendStatus(500)
+  })
 }
--- app/services/quiz.service.js
+++ app/services/quiz.service.js
@@ -1,18 +1,43 @@
-const mongodb = require('../mongodb')
+const mongodb = require("../mongodb")
 const conn = mongodb.connection
 const ObjectId = mongodb.ObjectId
 
 module.exports = {
-    create: _create
+  create: _create,
+  read: read
 }
 
-function _create (payload) {
-    console.log(payload)
-    return conn.db().collection('public-quizzes').insertOne(payload)
-        .then(result => result.insertedIds[0].toString())
-        .catch(onError)
+function read() {
+  return conn
+    .db()
+    .collection("public-quizzes")
+    .find({})
+    .toArray()
+    .then(quizzes => {
+      for (let i = 0; i < quizzes.length; i++) {
+        let quiz = quizzes[i]
+        quiz._id = quiz._id.toString()
+      }
+      return quizzes
+    })
+    .catch(err => {
+      return Promise.reject(err)
+    })
 }
 
-onError = (error) => {
-    console.error(error)
+function _create(payload) {
+  console.log(payload)
+  const doc = {
+    questions: payload.questions,
+    quizName: payload.quizName
+  }
+
+  return conn
+    .db()
+    .collection("public-quizzes")
+    .insertOne(doc)
+    .then(result => result.insertedId.toString())
+    .catch(err => {
+      return Promise.reject(err)
+    })
 }
